052231e8ae3e0a989fb77146bdb61dc6e67c1dab
KananAmir <amirovknn@gmail.com>
2024-06-03 10:28:20 +0400

    updated

--- 05.15.react-context-api/src/pages/Client/Home/index.jsx
+++ 05.15.react-context-api/src/pages/Client/Home/index.jsx
@@ -1,8 +1,38 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+
+import { Navigation } from "swiper/modules";
+
+import "./index.scss";
 const Home = () => {
   return (
     <div id="home">
-      <div className="container">
-        <div className="home">Home</div>
+      <div className="">
+        <div className="home">
+          <Swiper
+            navigation={true}
+            modules={[Navigation]}
+            loop={true}
+            spaceBetween={50}
+            slidesPerView={1}
+            onSlideChange={() => console.log("slide change")}
+            onSwiper={(swiper) => console.log(swiper)}
+            className="mySwiper"
+          >
+            <SwiperSlide className="slide slide1">
+              <div className="content">Tasty & Delicious Food</div>
+            </SwiperSlide>
+            <SwiperSlide className="slide slide2">Slide 2</SwiperSlide>
+            <SwiperSlide className="slide slide3">Slide 3</SwiperSlide>
+            <SwiperSlide className="slide slide1">Slide 1</SwiperSlide>
+            <SwiperSlide className="slide slide2">Slide 2</SwiperSlide>
+            <SwiperSlide className="slide slide3">Slide 3</SwiperSlide>
+            <SwiperSlide className="slide slide1">Slide 1</SwiperSlide>
+            <SwiperSlide className="slide slide2">Slide 2</SwiperSlide>
+            <SwiperSlide className="slide slide3">Slide 3</SwiperSlide>
+          </Swiper>
+        </div>
       </div>
     </div>
   );
